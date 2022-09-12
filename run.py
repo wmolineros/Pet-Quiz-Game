d495ce671664da0a77d0009369af6a5a0bc58810
wmolineros <wmolineros@gmail.com>
2022-09-12 15:41:41 +0000

    Add details to game option items 1-4

--- run.py
+++ run.py
@@ -8,6 +8,28 @@ def game_contents():
     The player will select either 'yes' or 'no' for a series of questions. 
     Depending on how many of either at the end, the game will suggest the best type
     of pet for the person's lifestyle. The player will be able to select play, discontinue, 
-    restart, or final result. 
+    restart, or advice for first time pet owners. 
     """
 print("\n")
+print("Press 1-4 to choose from below options:")
+print("1. Play \n2. Discontinue \n3. Restart \n4. Advice for first time pet owners. ")
+
+yes = True
+while True:
+    game_option = input("How would you like to proceed?\n").strip()
+    print(\n)
+
+    if game_option is == "1":
+        print("Let's keep moving!\n")
+        break 
+    elif game_option is == "2":
+        print("Reconsidered a life long friend? Sayonara!\n")
+        quit()
+    elif game_option is == "3":
+        print("Let's begin again!\n")
+        restart_game()
+    elif game_option is == "4":
+        display_advice()
+        game_option()
+    else:
+        print("This option is not valid. Please select between option 1-4.")
