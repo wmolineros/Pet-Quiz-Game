f695547a2efc9ccb5ab794bda9856ca5d6d73d7b
wmolineros <wmolineros@gmail.com>
2022-09-12 15:10:09 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+#Start of the quiz with a welcome message 
+print("Welcome to the Pet Quiz Game!")
+print("This game helps you find out what type of pet best suits your lifestyle.")
+
+def game_contents():
+    """
+    The player will select either 'yes' or 'no' for a series of questions. 
+    Depending on how many of either at the end, the game will suggest the best type
+    of pet for the person's lifestyle. The player will be able to select play, discontinue, 
+    restart, or final result. 
+    """
+print("\n")
