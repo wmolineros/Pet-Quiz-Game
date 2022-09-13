18ce16573eadb1c42930dac3cd6c210da3ccade2
wmolineros <wmolineros@gmail.com>
2022-09-13 19:51:16 +0000

    Review errors for quizgamepy

--- quizgame.py
+++ quizgame.py
@@ -1,18 +1,20 @@
-# this is the quiz game section of the game. 
+# this is the quiz game section of the game.
+
 
 def play_game():
     """
     Throughout this game section, the player will be going through
-    each statement and apply an answer to figure out what type of pet 
-    they should get depending on their lifestyle. The player will respond 'yes' or 'no'
-    to statement and based on these answers, will receive a recommendation."
+    each statement and apply an answer to figure out what type of pet
+    they should get depending on their lifestyle. The player will
+    respond 'yes' or 'no' to statement and based on these answers,
+    will receive a recommendation."
     """
-    score = 0 
-    
+    score = 0
+
     # first statement
 
     while True:
-        print("I have a lot of free time. Y/N?\n")
+        response1 = input("I have a lot of free time. Y/N?\n").strip()
         if response1.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
@@ -23,10 +25,10 @@ def play_game():
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
 
-   # second statement
+    # second statement
 
     while True:
-        print("I have a lot of free space. Y/N?\n")
+        response2 = input("I have a lot of free space. Y/N?\n").strip()
         if response2.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
@@ -37,120 +39,121 @@ def play_game():
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
 
-   # third statement
+    # third statement
 
     while True:
-        print("I am looking for companionship. Y/N?\n")
-        if response2.upper() == "Y":
+        response3 = input("I am looking for companionship. Y/N?\n").strip()
+        if response3.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
             break
-        elif response2.upper() == "N":
+        elif response3.upper() == "N":
             print("No worries!")
             break
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
-   
-   # fourth statement
+
+    # fourth statement
 
     while True:
-        print("I have outdoor space. Y/N?\n")
-        if response2.upper() == "Y":
+        response4 = input("I have outdoor space. Y/N?\n").strip()
+        if response4.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
             break
-        elif response2.upper() == "N":
+        elif response4.upper() == "N":
             print("Ok, let's see.")
             break
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
 
-   # fifth statement
+    # fifth statement
 
     while True:
-        print("I live in the country or suburb. Y/N?\n")
-        if response2.upper() == "Y":
+        response5 = input("I live in the country or suburb. Y/N?\n").strip()
+        if response5.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
             break
-        elif response2.upper() == "N":
+        elif response5.upper() == "N":
             print("Right, no bother!")
             break
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
 
-   # sixth statement
+    # sixth statement
 
     while True:
-        print("I am financially stable. Y/N?\n")
-        if response2.upper() == "Y":
+        response6 = input("I am financially stable. Y/N?\n").strip()
+        if response6.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
             break
-        elif response2.upper() == "N":
+        elif response6.upper() == "N":
             print("Ok, no problem. Something to consider.")
             break
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
 
-   # seventh statement
+    # seventh statement
 
     while True:
-        print("I like large animals. Y/N?\n")
-        if response2.upper() == "Y":
+        response7 = input("I like large animals. Y/N?\n").strip()
+        if response7.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
             break
-        elif response2.upper() == "N":
+        elif response7.upper() == "N":
             print("That's alright!")
             break
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
 
-   # eight statement
+    # eight statement
 
     while True:
-        print("Noise is ok with me and my neighbors. Y/N?\n")
-        if response2.upper() == "Y":
+        response8 = input("Noise is ok with me. Y/N?\n").strip()
+        if response8.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
             break
-        elif response2.upper() == "N":
+        elif response8.upper() == "N":
             print("No worries!")
             break
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
-   
-   # ninth statement
+
+    # ninth statement
 
     while True:
-        print("I wish to spend more time outside. Y/N?\n")
-        if response2.upper() == "Y":
+        response9 = input("I wish to spend more time outside. Y/N?\n").strip()
+        if response9.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
             break
-        elif response2.upper() == "N":
+        elif response9.upper() == "N":
             print("Ok, let's see.")
             break
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
 
-   # tenth statement
+    # tenth statement
 
     while True:
-        print("I wish to train my pet. Y/N?\n")
-        if response2.upper() == "Y":
+        response10 = input("I wish to train my pet. Y/N?\n").strip()
+        if response10.upper() == "Y":
             print("Closer to your dream pet!\n")
             score += 1
             break
-        elif response2.upper() == "N":
+        elif response10.upper() == "N":
             print("Right, no bother!")
             break
         else:
             print("Input is invalid. Please type 'Y' or 'N'.\n")
-    
-    # The score is tallied up and an answer provided. 
-    # The player also gets to see if they're recommended a low or high maintenance pet.
+
+    # The score is tallied up and an answer provided.
+    # The player also gets to see if they're recommended
+    # a low or high maintenance pet.
 
     if score < 6:
         print("A low maintenance pet is best for you; such as a cat or fish.")
@@ -162,7 +165,3 @@ def play_game():
     else:
         print("It's great to consider a pet. All breeds are fascinating!")
         return score
-    
-
-
-
--- run.py
+++ run.py
@@ -1,29 +1,32 @@
 import quizgame
 
-#Start of the quiz with a welcome message 
+# Start of the quiz with a welcome message
 print("Welcome to the Pet Quiz Game!")
-print("This game helps you find out what type of pet best suits your lifestyle.")
+print("This game helps you find out what type of")
+print("pet best suits your lifestyle.")
+
 
 def game_contents():
     """
-    The player will select either 'yes' or 'no' for a series of statements. 
-    Depending on how many of either at the end, the game will suggest the best type
-    of pet for the person's lifestyle. The player will be able to select play, discontinue, 
-    restart, or advice for first time pet owners. 
+    The player will select either 'yes' or 'no' for a series of statements.
+    Depending on how many of either at the end, the game will suggest the
+    best type of pet for the person's lifestyle. The player will be able to
+    select play, discontinue, restart, or advice for first time pet owners.
     """
     print("\n")
     print("Press 1-4 to choose from below options:")
-    print("1. Play \n2. Discontinue \n3. Restart \n4. Advice for first time pet owners.\n")
-    
+    print("1. Play \n2. Discontinue \n3. Restart \n4. Advice for first time")
+    print("pet owners.\n")
+
     while True:
-        game_option = input("How would you like to proceed? Type one number from 1-4.\n").strip()
-        print("\n")
-        
+        game_option = input("How would you like to proceed?\n").strip()
+        print("Type one number from 1-4.\n")
+
         if game_option == "1":
             print("Let's keep moving!\n")
             break
         elif game_option == "2":
-            print("Reconsidered a life long friend? Sayonara!\n")
+            print("Reconsidered a life long friend? No problem. Sayonara!\n")
             quit()
         elif game_option == "3":
             print("Let's begin again!\n")
@@ -31,7 +34,9 @@ def game_contents():
         elif game_option == "4":
             display_advice_details()
         else:
-            print("This option is not valid. Please select between option 1-4.")
+            print("This option is not valid. Please select between")
+            print("option 1-4.")
+
 
 def player_name():
     """
@@ -43,9 +48,9 @@ def player_name():
         print("Do not input any numbers.")
         print("Maximum of 10 characters.")
         print("Blank spaces will be removed.")
-        
-        player_name = input("Please enter your  name: \n")
-        
+
+        player_name = input("Please enter your name: \n")
+
         if verify_player_name(player_name):
             print("\n")
             print(f"Aloha {player_name}, let's begin the quiz game!\n")
@@ -53,10 +58,10 @@ def player_name():
             print(f"choose the best pet for your lifestyle, {player_name}!\n")
             print("Type in Y/N or y/n to answer each statement.\n")
             print("Can't wait to see your result!")
-            data = quizgame.play_game()
-   
+
     verify_player_name(player_name)
     return player_name().strip
+
 
 def verify_player_name(player_name):
     """
@@ -64,7 +69,7 @@ def verify_player_name(player_name):
     A value error will be raised if it is over 10 characters long, no name
     entered or contains any numbers.
     """
-    try: 
+    try:
         if not player_name:
             raise ValueError("Please enter a valid player name.")
         if len(player_name) > 10:
@@ -76,13 +81,15 @@ def verify_player_name(player_name):
 
     return True
 
+
 def restart_game():
     """
     This section allows the player to play the game multiple times
     """
     while True:
         data = quizgame.play_game()
-        game_menu()
+        game_option()
+
 
 def display_advice_details():
     """
@@ -99,17 +106,23 @@ def display_advice_details():
         print("demanding or requires a lot of maintenance. Pets to consider")
         print("include house plants, fish, rock. You want to think about the")
         print("well being of the pet you're brining into your home and ask")
-        print("yourself if you would be able to enhance their life; not deplete it")
+        print("yourself if you would be able to enhance their life;")
+        print("not deplete it.\n")
+        print("Throughout this game, you will notice that depending on your")
+        print("response for each statement you will receive a suggestion for")
+        print("the type of animal (low or high maintenance) that you should")
+        print("get.\n Low maintenance pets include a cat, plant fish, hamster")
+        print("or small bird.\n")
+        print("A high maintenance pet includes a dog, horse, rabbit, large")
+        print("or turtle (commonly confused as a low maintenance pet).")
+        break
+
 
 def main():
     """
-    This main feature runs all functions for the game. 
+    This main feature runs all functions for the game.
     """
     game_contents()
     player_name()
 
 main()
-
-
-
-    
